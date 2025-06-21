512656df56117309bd66632c1879e73c7d35bc8d
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2025-06-22 00:14:55 +0800

    Update passwall2.lua

--- luci-app-passwall2/luasrc/controller/passwall2.lua
+++ luci-app-passwall2/luasrc/controller/passwall2.lua
@@ -79,6 +79,7 @@ function index()
 	entry({"admin", "services", appname, "update_rules"}, call("update_rules")).leaf = true
 	entry({"admin", "services", appname, "subscribe_del_node"}, call("subscribe_del_node")).leaf = true
 	entry({"admin", "services", appname, "subscribe_del_all"}, call("subscribe_del_all")).leaf = true
+	entry({"admin", "services", appname, "ip"}, call('check_ip')).leaf = true
 
 	--[[Components update]]
 	entry({"admin", "services", appname, "check_passwall2"}, call("app_check")).leaf = true
